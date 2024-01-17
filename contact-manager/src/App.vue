b80aab6823b3f885c929c2a2626beeade614e1f1
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-17 14:25:04 +0100

    Création d'un Single File Component

--- contact-manager/src/App.vue
+++ contact-manager/src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ContactForm />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContactForm from './components/ContactForm.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    ContactForm
   }
-}
+};
 </script>
 
 <style>
