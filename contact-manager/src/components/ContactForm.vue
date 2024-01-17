9a40241c884f3783714587be954ec352d4471526
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-17 14:47:17 +0100

    Lier les champs d'un formulaire aux données d'un component

--- contact-manager/src/components/ContactForm.vue
+++ contact-manager/src/components/ContactForm.vue
@@ -1,12 +1,28 @@
 <template>
     <div>
-        <h2>Contact Form</h2>
+        <form>
+            <input type="text" placeholder="prénom" v-model="contact.firstName"><br>
+            <input type="text" placeholder="nom" v-model="contact.lastName"><br>
+            <input type="text" placeholder="email" v-model="contact.email"><br>
+            <input type="text" placeholder="téléphone" v-model="contact.phone"><br>
+            <button type="submit">créer</button>
+        </form>
     </div>
 </template>
 
 <script>
+// snippet "vdata"
     export default {
-        
+        data() {
+            return {
+                contact: {
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    phone: ""
+                }
+            };
+        }
     }
 </script>
 
